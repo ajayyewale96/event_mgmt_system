335f8c8c2c06ffcb1dd9b756332ef1735dbaa21f
Ajay Yewale <ajayyewale8@gmail.com>
2025-08-10 09:46:57 +0530

    added the postgres container,psycopg2-bnary and docker compose for separtion of services

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from flask_smorest import Api
+from flask_smorest import Api,abort
+from sqlalchemy.exc import SQLAlchemyError
+import os
+from time import sleep
 import models
 from resources.event import blp as EventBluePrint
 from db import db
-import os
 
-def create_app(db_url=None):
+
+def create_app(db_url='postgresql+psycopg2://postgres:postgres@postgres/event_mgmt_system'):
     app=Flask(__name__)
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Event Management REST API"
@@ -18,9 +21,19 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     api=Api(app)
-    
+    db_connection_retry=5
     with app.app_context():
-        db.create_all()
+        while db_connection_retry > 0:
+            try:
+                db.create_all()
+                break
+            except SQLAlchemyError:
+                sleep(60//db_connection_retry)
+                db_connection_retry-=1
+                
+        if db_connection_retry <=0:
+            abort(500,message='Error occured while connecting the db')
+            
     api.register_blueprint(EventBluePrint)
 
     return app
